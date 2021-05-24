73b2fe9c9dcf4567552a4e27a2d88a035f821c82
Jonatan Pereyra Acevedo <jpereyraacevedo@gmail.com>
2021-05-24 19:45:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// var numero=prompt("Ingrese aqui su primer numero")
+// var numero2=prompt("Ingrese aqui se segundo numero")
+// var operacion=prompt("¿Qué operación desea realizar?")
+// var resultado=
+// alert ("Test1")
